f3511a0cdec72f17bef1a4d2a54ad2a18f9c7b0e
nikhilgit02 <srivastavanikhil107@gmail.com>
2023-10-27 14:20:42 +0530

    added hard coded card component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import StaticMain from './components/StaticMain';
+import Card from './components/Card';
 
 export default function App() {
   return (
     <div>
       <Navbar/>
       <StaticMain/>
+      <Card/>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,18 @@
 import React from "react";
 import image from "../image 12.png"
+import star from "../Star 1.png"
 
 export default function Card(){
     return(
         <section>
-            <div>
-                <img src={image}/>
+            <div className="card-base">
+                <img src={image} className="card-main-image"/>
+                <div className="rating">
+                <img src={star} className="star-img"/>
+                <p>5.0 (6) . USA</p>
+                </div>
+                <p className="card-details">Life lessons with Katie Zaferes</p>
+                <p className="card-details"><b>From $136 </b>/ person</p>
             </div>
         </section>
     )
