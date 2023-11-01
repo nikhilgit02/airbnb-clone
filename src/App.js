5e37b6631756b3c1dcab5183ae9a432cc032064f
nikhilgit02 <srivastavanikhil107@gmail.com>
2023-11-01 22:09:18 +0530

    added card component using props but still hard coded data inside the component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import StaticMain from './components/StaticMain';
 import Card from './components/Card';
 
 export default function App() {
+ 
   return (
     <div>
       <Navbar/>
       <StaticMain/>
-      <Card/>
+      <Card
+      img = "card1image.png"
+      rating ={5.0}
+      reviewCount={6}
+      country="USA"
+      title="Life Lesson with Katie Zaffers"
+      price={136}
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,17 @@
 import React from "react";
-import image from "../image 12.png"
 import star from "../Star 1.png"
 
-export default function Card(){
+export default function Card(props){
     return(
         <section>
             <div className="card-base">
-                <img src={image} className="card-main-image"/>
+                <img src={`images/${props.img}`} className="card-main-image"/>
                 <div className="rating">
                 <img src={star} className="star-img"/>
-                <p>5.0 (6) . USA</p>
+                <p>{props.rating} ({props.reviewCount}) . {props.country}</p>
                 </div>
-                <p className="card-details">Life lessons with Katie Zaferes</p>
-                <p className="card-details"><b>From $136 </b>/ person</p>
+                <p className="card-details">{props.title}</p>
+                <p className="card-details"><b>From ${props.price} </b>/ person</p>
             </div>
         </section>
     )
